fa8fc453ab6541030597f513992614aebbf2dea1
Dustin Kimmel <dkimmel@gmail.com>
2016-08-29 12:15:37 -0700

    server static files from client, build index.html, src jquery in page

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+var $ = require('jQuery');
 var express = require('express');
+
 var app = express();
 
+
+// var getKillingsFromAPI = function(){};
+app.use(express.static('client'));
+app.use('/scripts', express.static(__dirname + '/node_modules/jquery/dist/'));
+
 app.get('/', function(request, response){
-  response.end('Hello, world!');
+	
+	console.log('About to GET');
+  $.get("https://thecountedapi.com/api/counted/?state=CA", function(){
+  })
+  .done(function(data) {
+    response.end(JSON.stringify(data));
+  })
+  .fail(function(err) {
+  	console.error(err);
+  });
+
 });
 
 app.listen(3000, function() {
